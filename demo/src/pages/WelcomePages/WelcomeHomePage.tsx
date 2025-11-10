6012ccbd2e1cb195244299c90ec7233d6e1208f7
Frank Overland <frank.overland@gravitate.energy>
2025-11-10 13:17:08 -0800

    WIP on main
    quick test

--- demo/src/pages/WelcomePages/WelcomeHomePage.tsx
+++ demo/src/pages/WelcomePages/WelcomeHomePage.tsx
@@ -40,7 +40,7 @@ export function WelcomeHomePage() {
       {/* Header */}
       <Vertical className="mb-2">
         <Texto category="heading" className="mb-2">
-          Excalibrr Demo Showcase Frank was here!!!
+          Excalibrr Demo Showcase Frank Does it Again!!!
         </Texto>
         <Texto category="p1" style={{ lineHeight: "1.6" }}>
           Interactive demonstrations of Excalibrr components using production
